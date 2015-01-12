0d6c0b15151b9e22d2722c73b66ac1e34330f0d9
Hariharan <hariharan93m@gmail.com>
2015-01-12 17:10:52 +0530

    first commit

---
+++ Nextfile.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://127.0.0.1:27017/test1');
+
+exports.getHtml=function(req,res) {
+	res.render("index");
+};
+var ps=new mongoose.Schema({
+	username:"string"
+},{ 
+	collection : 'unametable'
+});
+var cols = mongoose.model('unametable', ps);
+exports.getJson=function(req,res) {
+	var name=req.query['username'];
+	console.log(name);
+	
+
+	Abc(function (err,resultdata) {
+	
+		if(err) {
+		res.jsonp("error");
+		} else {
+			res.jsonp(resultdata)
+		}
+	});
+	function Abc(mycallback) {
+		
+		cols.findOne({"username":new RegExp('^'+name+'$', "i")},function(err,col) {
+			mycallback(err,col);
+			//console.log("This"+col);
+		
+		});
+		
+	}
+	
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+var express  = require('express');
+var app      = express();
+
+var nextFile = require('./Nextfile');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',nextFile.getHtml);
+app.get('/getJson',nextFile.getJson);
+
+var server = app.listen(3020, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
+
